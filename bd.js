ccb9d87268cc3c7c23d69c5dea8616b5ec5691e5
alissonreifur <alissonreifur@gmail.com>
2022-11-03 11:59:09 -0300

    Atualização de idade

--- bd.js
+++ bd.js
@@ -1,15 +1,16 @@
 var bd = openDatabase("meuBD", "1.0", "Meu banco de daos", 4080);
 
 bd.transaction(function (criar) {
-    criar.executeSql("CREATE TABLE formulario (nome TEXT)");
+    criar.executeSql("CREATE TABLE formulario (nome TEXT, idade INTEGER)");
 });
 
-function salvarNome() {
+function salvarInfo() {
     const nomeUsuario = document.getElementById("nome-usuario").value;
+    const idadeUsuario = parseInt(document.getElementById("idade-usuario").value);
 
     bd.transaction(function (inserir) {
-        inserir.executeSql("INSERT INTO formulario (nome) VALUES (?)", [
-            nomeUsuario,
+        inserir.executeSql("INSERT INTO formulario (nome, idade) VALUES (?, ?)", [
+            nomeUsuario, idadeUsuario
         ]);
     });
 }
