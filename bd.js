6d2ba811323970408823ce090733b5cae4c14634
alissonreifur <alissonreifur@gmail.com>
2022-11-11 11:49:22 -0300

    adicionei botoes e banco de dados

--- bd.js
+++ bd.js
@@ -4,34 +4,40 @@ let listaNomes = [];
 let listaIdade = [];
 let listaCPF = [];
 let listaNascimento = [];
+let listaTelefone = [];
+let listaEmail = [];
 
 
 bd.transaction(function (criar) {
-    criar.executeSql("CREATE TABLE formulario (nome TEXT, idade INTEGER, cpf INTEGER, nascimento DATE)");
+    criar.executeSql(
+        "CREATE TABLE formulario (nome TEXT, idade INTEGER, cpf INTEGER(11), nascimento DATE ,  telefone INTEGER, email TEXT)");
 });
 
 function salvarInfo() {
     const nomeUsuario = document.getElementById("nome-usuario").value.toUpperCase();
-    const idadeUsuario = parseInt(document.getElementById("idade-usuario")).value
-    const cpfUsuario = parseInt(document.getElementById("cpf-usuario")).value
-    const nascimentoUsuario = parseInt(
-                document.getElementById("nascimento-usuario").value
+    const idadeUsuario = parseInt(document.getElementById("idade-usuario").value);
+    const cpfUsuario = parseInt(document.getElementById("cpf-usuario").value);
+    const nascimentoUsuario = document.getElementById("nascimento-usuario").value;
+    const telefoneUsuario = document.getElementById("telefone-usuario");
+    const emailUsuario = document.getElementById("email-usuario").value;
 
-    );
+    console.log(nomeUsuario, idadeUsuario);
     if (nomeUsuario === "" || isNaN(idadeUsuario)) {
         alert("Faltam informações");
         return false;
     }
     bd.transaction(function (inserir) {
         inserir.executeSql(
-            "INSERT INTO formulario (nome, idade, cpf, nascimento) VALUES (?, ?, ?, ?)",
-            [nomeUsuario, idadeUsuario]
+            "INSERT INTO formulario (nome, idade, cpf, nascimento, telefone, email) VALUES (?, ?, ?, ?, ?, ?)",
+            [nomeUsuario, idadeUsuario, cpfUsuario, nascimentoUsuario, telefoneUsuario, emailUsuario]
         );
     });
     document.getElementById("nome-usuario").value = "";
     document.getElementById("idade-usuario").value = "";
     document.getElementById("cpf-usuario").value = "";
     document.getElementById("nascimento-usuario").value = "";
+    document.getElementById("telefone-usuario").value = "";
+    document.getElementById("email-usuario").value = "";
 }
 
 function pesquisaPorNome() {
@@ -50,18 +56,25 @@ function pesquisaPorNome() {
                 const idade = resultados.rows.item(tamanho - 1).idade;
                 const cpf = resultados.rows.item(tamanho - 1).cpf;
                 const nascimento = resultados.rows.item(tamanho - 1).nascimento;
+                const telefone = resultados.rows.item(tamanho - 1).telefone;
+                const email= resultados.rows.item(tamanho - 1).email;
 
                 document.getElementById("pesquisa-nome-usuario").value = nome;
-                document.getElementById("resultado-pesquisa").value = idade;
+                document.getElementById("resultado-idade-pesquisa").value = idade;
                 document.getElementById("resultado-cpf-pesquisa").value = cpf;
-                document.getElementById("resultado-nascimento-pesquisa").value = nascimento;
+                document.getElementById("resultado-nascimento-pesquisa").value =
+                    nascimento;
+                document.getElementById("resultado-telefone-pesquisa").value = telefone;
+                document.getElementById("resultado-email-pesquisa").value = email;
             }
         );
     });
     document.getElementById("pesquisa-nome-usuario").value = "";
-    document.getElementById("resultado-pesquisa").value = "";
+    document.getElementById("resultado-idade-pesquisa").value = "";
     document.getElementById("resultado-cpf-pesquisa").value = "";
     document.getElementById("resultado-nascimento-pesquisa").value = "";
+    document.getElementById("resultado-telefone-pesquisa").value = "";
+    document.getElementById("resultado-email-pesquisa").value = "";
 }
 
 function exibeBD() {
@@ -78,7 +91,7 @@ function exibeBD() {
                     item = resultados.rows.item(i);
                     document.getElementById(
                         "lista-bd"
-                    ).innerHTML += `<p onclick="mostrarCartaoAltera('${item.nome}' , ${item.idade})">Nome: ${item.nome}, ${item.idade} anos. </p>`;
+                    ).innerHTML += `<p onclick="mostrarCartaoAltera('${item.nome}' , ${item.idade} , ${item.cpf} , '${item.nascimento} , ${item.telefone}, '${item.email}' )">Nome: ${item.nome}, ${item.idade} anos. , ${item.cpf} , ${item.nascimento} ,${item.telefone}, '${item.email}' </p>`;
                 }
             }
         );
@@ -90,15 +103,20 @@ function limpaBDDiv() {
 }
 
 function alteraInfo() {
-    const novoNome = document.getElementById("nome-alteracao").value
-    const novaIdade = parseInt(document.getElementById("idade-alteracao").value)
-    const novoCpf = parseInt(document.getElementById("cpf-alteracao").value)
-    const novoNascimento = parseInt(document.getElementById("nascimento-alteracao").value)
-    
-    
-
-    bd.transaction(function(altera){
-        altera.executeSql(`UPDATE formulario SET nome="${novoNome}", idade=${novaIdade} WHERE nome="${nomeAtualParaEditar}" AND idade=${idadeAtualParaEditar}`);
+    const novoNome = document.getElementById("nome-alteracao").value;
+    const novaIdade = parseInt(document.getElementById("idade-alteracao").value);
+    const novoCpf = parseInt(document.getElementById("cpf-alteracao").value);
+    const novoNascimento = parseInt(document.getElementById("nascimento-alteracao").value );
+    const novoTelefone = parseInt(document.getElementById("telefone-alteracao").value);
+    const novoEmail = parseInt(document.getElementById("email-alteracao").value);
+    bd.transaction(function (altera) {
+        altera.executeSql(
+            `UPDATE formulario SET nome="${novoNome}", idade=${novaIdade}, cpf=${novoCpf}, nascimento="${novoNascimento}" , telefone=${novoTelefone}, email="${novoEmail}"  WHERE nome="${nomeAtualParaEditar}" AND idade=${idadeAtualParaEditar} AND cpf=${cpfAtualParaEditar} AND nascimento="${nascimentoAtualParaEditar}" AND telefone=${telefoneAtualParaEditar} AND email="${emailAtualParaEditar}"`
+        );
     });
     exibeBD();
 }
+
+function excluiInfor() {
+    
+ }
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let nomeAtualParaEditar;
 let idadeAtualParaEditar;
 let cpfAtualParaEditar;
 let nascimentoAtualParaEditar;
+let telefoneAtualParaEditar;
+let emailAtualParaEditar;
 
 function converteParaFahrenheit() {
     const temperaturaCelsius = parseFloat(
@@ -13,16 +15,22 @@ function converteParaFahrenheit() {
         temperaturaFahrenheit.toFixed(2);
 }
 
-function mostrarCartaoAltera(nome, idade) {
+function mostrarCartaoAltera(nome, idade, cpf, nascimento, telefone, email) {
     document.getElementById("nome-alteracao").value = nome;
     nomeAtualParaEditar = nome;
 
     document.getElementById("idade-alteracao").value = idade;
     idadeAtualParaEditar = parseInt(idade);
 
-    document.getElementById("cpf-alteração").valeu = cpf
-    cpfAtualParaEditar = parseInt(cpf);
+    document.getElementById("cpf-alteracao").value = cpf;
+    cpfAtualParaEditar = cpf;
 
-    document.getElementById("nascimento-alteracao").value = nascimento
+    document.getElementById("nascimento-alteracao").value = nascimento;
     nascimentoAtualParaEditar = nascimento;
+
+    document.getElementById("telefone-alteracao").value = telefone;
+    telefoneAtualParaEditar = parseInt(telefone);
+
+    document.getElementById("email-alteracao").value = email;
+    emailAtualParaEditar = email;
 }
